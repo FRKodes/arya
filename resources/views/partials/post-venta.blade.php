1c82315fe92c49d0d96c66f6064ca8fc3ece3941
frkodes <frkalderon@gmail.com>
2016-08-08 11:05:40 -0500

    CSS fix on who we are and so

--- resources/views/partials/post-venta.blade.php
+++ resources/views/partials/post-venta.blade.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<h2 class="text-center mayus hidden-xs">El mejor servicio post-venta</h2>
 		<div class="col-sm-6 image"><img src="/images/post_venta.jpg" class="hidden-sm hidden-md hidden-lg" alt="El mejor servicio Post-venta"></div>
-		<div class="col-sm-6">
+		<div class="col-sm-6 text">
 			<h2 class="text-center mayus hidden-sm hidden-md hidden-lg">El mejor servicio post-venta</h2>
 			<p>Con ARYA contarás con el mejor servicio postventa del mercado. </p>
 			<p>En  ARYA te garantizamos la mayor calidad, es por eso que tendrás el respaldo de nuestra GARANTIA ARYA,  que te dará la tranquilidad de que a tu hogar no le fallará nada.</p>
--- resources/views/partials/quienes-somos.blade.php
+++ resources/views/partials/quienes-somos.blade.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<h2 class="text-center mayus hidden-xs">¿Quiénes somos?</h2>
 		<div class="col-sm-6 image"><img src="/images/familia.jpg" class="hidden-sm hidden-md hidden-lg" alt="Foto familia feliz"></div>
-		<div class="col-sm-6">
+		<div class="col-sm-6 text">
 			<h2 class="text-center mayus hidden-sm hidden-md hidden-lg">¿Quiénes somos?</h2>
 			<p>ARYA DESARROLLOS, proyecta y hace realidad tu casa ideal, a tu medida y presupuesto, de una manera práctica y sencilla.</p>
 			<p>Te llevamos de la mano, desde la selección de tu terreno, hasta los últimos detalles, para crear el hogar que siempre deseaste.</p>
